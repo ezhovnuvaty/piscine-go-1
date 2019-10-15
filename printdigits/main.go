8aa89a2412ad9383f07293689589520db5250e2d
Adam <kuzikov.dev@protonmail.com>
2019-10-16 05:09:08 +0600

    digits fix 4

--- printdigits/main.go
+++ printdigits/main.go
@@ -1,10 +1,10 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func main() {
-	for i := 0; i <= 10; i++ {
-		fmt.Print(i)
+	for i := '0'; i <= '9'; i++ {
+		z01.PrintRune(i)
 	}
-	//z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
