d70faadfcbc9a7e84e4480ade0a5c067467d711a
Adam <kuzikov.dev@protonmail.com>
2019-10-15 13:00:56 +0600

    I'am said, Iam Batman!

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -M --raw-output 'map(select(.id == 70)) | .[].name'
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -M 'map(select(.id == 70)) | .[].name'
 
